e15380a3a9006a520f9c1535fd1986f6e92d4acc
tcn453 <79690881+tcn453@users.noreply.github.com>
2021-03-31 21:52:25 +0200

    Added Axios defaults

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,16 +5,25 @@ import { Activity } from '../models/activity';
 import Navbar from './navbar';
 import ActivityDashboard from '../../features/Activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponents';
 
 
 function App() {
     const [activities, setActivities] = useState<Activity[]>([])
     const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
     const [editMode, setEditMode] = useState(false);
+    const [loading, setLoading] = useState(true);
   
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
+    agent.Activities.list().then(response => {
+        let activities: Activity[] = [];
+        response.forEach(activity => {
+            activity.date = activity.date.split('T')[0];
+            activities.push(activity);
+        })
+      setActivities(activities);
+        setLoading(false);
     })
   },[])
     
@@ -44,7 +53,7 @@ function App() {
   function handleDeleteActivity(id:string){
         setActivities([...activities.filter(x => x.id !== id)])
     }
-  
+  if (loading) return <LoadingComponent content={'Loading app'} />
   return (
     <>
         <Navbar openForm={handleFormOpen}/>
--- client-app/src/features/Activities/form/ActivityForm.tsx
+++ client-app/src/features/Activities/form/ActivityForm.tsx
@@ -35,7 +35,7 @@ export default function ActivityForm({closeForm, activity : selectedActivity, cr
             <Form onSubmit={handleSubmit} autoComplete={"off"}>
                 <Form.Input placeholder={"Title"} value={activity.title} name="title" onChange={handleInputChange}/>
                 <Form.TextArea placeholder={"Description"} value={activity.description} name="description" onChange={handleInputChange} />
-                <Form.Input placeholder={"Date"} value={activity.date} name="date" onChange={handleInputChange}/>
+                <Form.Input type={"date"} placeholder={"Date"} value={activity.date} name="date" onChange={handleInputChange}/>
                 <Form.Input placeholder={"Category"} value={activity.category} name="category" onChange={handleInputChange}/>
                 <Form.Input placeholder={"City"} value={activity.city} name="city" onChange={handleInputChange}/>
                 <Form.Input placeholder={"Venue"} value={activity.venue} name="venue" onChange={handleInputChange}/>
